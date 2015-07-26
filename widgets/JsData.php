6a5d6e55d2aea8edabde5677acda9f65cf97f1d9
sjoorm <sjoorm1@gmail.com>
2015-07-26 20:27:26 +0600

    feature: JsData widget

---
+++ widgets/JsData.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Alexey Tishchenko
+ * Email: tischenkoalexey1@gmail.com
+ * oDesk: https://www.odesk.com/users/%7E01ad7ed1a6ade4e02e
+ * Date: 13.11.14
+ */
+namespace common\widgets;
+use sjoorm\yii\components\base\PageView;
+use yii\base\Widget;
+/**
+ * Class JsData helps to transfer data from PHP to JS code
+ * @package common\widgets
+ */
+class JsData extends Widget {
+
+    /** @var array text messages */
+    public $messages = [];
+    /** @var array links */
+    public $urls = [];
+    /** @var string custom variables */
+    public $variables = [];
+
+    /** @var string script that will be generated and inserted into the page code */
+    private $script;
+
+    /** @inheritdoc */
+    public function init() {
+        $yiiMessages = \Yii::$app->i18n;
+        $messageSource = $yiiMessages->getMessageSource('js');
+        /** @var \sjoorm\yii\components\base\PhpMessageSource $messageSource */
+        $errors = $messageSource->getMessages('js');
+        $dataTable = $messageSource->getMessages('dataTable');
+        $this->messages = array_merge($errors, $dataTable, $this->messages);
+        $this->script = 'window.pageMessages = ' . json_encode($this->messages) . ';
+            window.pageUrls = ' . json_encode($this->urls) . ';
+            window.pageVariables = ' . json_encode($this->variables) . ';';
+    }
+
+    /** @inheritdoc */
+    public function run() {
+        $this->view->registerJs($this->script, PageView::POS_BEGIN, 'jsData');
+    }
+}
